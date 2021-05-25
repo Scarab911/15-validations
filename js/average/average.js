b2c165c0dd7de02f175a5fa39eef937e2b9041c7
Paulius B <Paulspremium@gmail.com>
2021-05-25 19:26:08 +0300

    average

--- js/average/average.js
+++ js/average/average.js
@@ -9,3 +9,31 @@ function average(list) {
 module.exports = average;
 
 // return list.reduce((t, s) => t + s, 0) / list.length;
+
+
+
+// DESTYTOJO KODAS
+
+
+
+// const average = list => {
+//     if (list === undefined ||
+//         !Array.isArray(list) ||
+//         list.length === 0) {
+//         return false;
+//     }
+
+//     let totalSum = 0;
+//     let validNumbersCount = 0;
+
+//     for (const number of list) {
+//         if (typeof number === 'number' && !isNaN(number)) {
+//             totalSum += number;
+//             validNumbersCount++;
+//         }
+//     }
+
+//     return totalSum / validNumbersCount;
+// };
+
+// module.exports = average;
